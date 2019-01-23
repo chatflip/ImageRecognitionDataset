f22200833a34f28b543190b9d0d1ad33d6119d85
chatflip <oo.chat.flip@gmail.com>
2019-01-23 16:32:14 +0900

    caltech256 test

---
+++ caltech256/py/symblic_caltech256.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import random
+import argparse
+import numpy as np
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    src_root = root+"/datasets/256_ObjectCategories/"
+    dst_root = root+"/data/caltech256/"
+    for num_subset in range(0,10):
+        if not os.path.exists(dst_root+"subset"+str(num_subset)):
+            os.makedirs(dst_root+"subset"+str(num_subset))
+            os.makedirs(dst_root+"subset"+str(num_subset)+"/train")
+            os.makedirs(dst_root+"subset"+str(num_subset)+"/test")
+        class_names = os.listdir(src_root)
+        class_names.sort()
+        for class_name in class_names:
+            if "BACKGROUND" in class_name:
+                continue
+            elif not os.path.exists(dst_root+"subset"+str(num_subset)+"/train/"+class_name):
+                os.makedirs(dst_root+"subset"+str(num_subset)+"/train/"+class_name)
+                os.makedirs(dst_root+"subset"+str(num_subset)+"/test/"+class_name)
+        train_lists = np.genfromtxt(root+"/csv/caltech256_train_subset"+str(num_subset)+".csv",dtype=np.str)
+        test_lists = np.genfromtxt(root+"/csv/caltech256_test_subset"+str(num_subset)+".csv",dtype=np.str)
+        dst_subset_root = dst_root+"subset"+str(num_subset)+"/"
+        for train_list in train_lists:
+            os.symlink(src_root+train_list, dst_subset_root+"train/"+train_list)
+        for test_list in test_lists:
+            os.symlink(src_root+test_list, dst_subset_root+"test/"+test_list)
