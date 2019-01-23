9bc9e3a8b9aedf66b4f38c6a25055ed63d16d26a
chatflip <oo.chat.flip@gmail.com>
2019-01-23 14:57:56 +0900

    fashion MNIST test

---
+++ fashionMNIST/scripts/download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz
+python py/parser.py
