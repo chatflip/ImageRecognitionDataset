c2753bedcb9ea9855657ac371ca72fbcaf55d515
chatflip <oo.chat.flip@gmail.com>
2019-01-23 19:28:40 +0900

    caltech256

--- caltech256/scripts/download.sh
+++ caltech256/scripts/download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget http://www.vision.caltech.edu/Image_Datasets/Caltech256/256_ObjectCategories.tar
-tar -zxvf 101_ObjectCategories.tar.gz -C ./datasets/
-python py/make_randomlist_caltech101.py
-python py/symblic_caltech101.py
+tar -xvf 256_ObjectCategories.tar -C ./datasets/
+python py/make_randomlist_caltech256.py
+python py/symblic_caltech256.py
