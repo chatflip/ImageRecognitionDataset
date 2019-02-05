9a5da57574ea14c0db31af6e899ca01c46bf96c4
Kazushige Okayasu <oo.chat.flip@gmail.com>
2019-02-05 19:36:50 +0900

    fix scripts

--- caltech101/scripts/start.sh
+++ caltech101/scripts/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-mkdir -p csv
 mkdir -p data
 mkdir -p datasets
 
 wget http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
 tar -zxvf 101_ObjectCategories.tar.gz -C ./datasets/
-python py/randomlist.py
+#python py/randomlist.py
 python py/symblic.py
--- caltech256/scripts/start.sh
+++ caltech256/scripts/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-mkdir -p csv
 mkdir -p data
 mkdir -p datasets
 
 wget http://www.vision.caltech.edu/Image_Datasets/Caltech256/256_ObjectCategories.tar
 tar -xvf 256_ObjectCategories.tar -C ./datasets/
-python py/randomlist.py
+#python py/randomlist.py
 python py/symblic.py
