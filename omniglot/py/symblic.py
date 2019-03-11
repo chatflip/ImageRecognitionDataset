4ead4a63da05afc28e8895ee78b81d6f35b4e76b
sos <oo.chat.flip@gmail.com>
2019-03-11 14:59:57 +0900

    fix data split

--- omniglot/py/symblic.py
+++ omniglot/py/symblic.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     root = os.getcwd()
     src_root = root+"/data/original/"
     dst_root = root+"/data/"
-    for num_subset in range(5):
+    for num_subset in range(20):
         if not os.path.exists(dst_root+"subset"+str(num_subset)):
             os.makedirs(dst_root+"subset"+str(num_subset))
             os.makedirs(dst_root+"subset"+str(num_subset)+"/train")
@@ -18,10 +18,10 @@ if __name__ == '__main__':
             if not os.path.exists(dst_root+"subset"+str(num_subset)+"/train/"+class_name):
                 os.makedirs(dst_root+"subset"+str(num_subset)+"/train/"+class_name)
                 os.makedirs(dst_root+"subset"+str(num_subset)+"/test/"+class_name)
-        train_lists = np.genfromtxt(root+"/csv/omniglot_train_subset"+str(num_subset)+".csv", dtype=np.str)
-        test_lists = np.genfromtxt(root+"/csv/omniglot_test_subset"+str(num_subset)+".csv", dtype=np.str)
-        dst_subset_root = dst_root+"subset"+str(num_subset)+"/"
-        for train_list in train_lists:
-            os.symlink(src_root+train_list, dst_subset_root+"train/"+train_list)
-        for test_list in test_lists:
-            os.symlink(src_root+test_list, dst_subset_root+"test/"+test_list)
+            file_names = os.listdir(src_root+class_name+"/")
+            dst_subset_root = dst_root+"subset"+str(num_subset)+"/"
+            for file_name in file_names:
+                if "{:02d}.png".format(num_subset+1) in file_name:
+                    os.symlink(src_root+class_name+"/"+file_name, dst_subset_root+"train/"+class_name+"/"+file_name)
+                else:
+                    os.symlink(src_root+class_name+"/"+file_name, dst_subset_root+"test/"+class_name+"/"+file_name)
--- omniglot/scripts/start.sh
+++ omniglot/scripts/start.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 mkdir -p data
-mkdir -p csv
 wget https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_background.zip
 wget https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_evaluation.zip
 unzip images_background.zip
 unzip images_evaluation.zip
 python py/parser.py
-python py/randomlist.py
 python py/symblic.py
 rm -rf images_background
 rm -rf images_evaluation
