e6bd3ed5b253c7c88a217c085ba95c84112123ac
Kazushige Okayasu <oo.chat.flip@gmail.com>
2019-03-22 18:00:55 +0900

    fix omniglot

--- omniglot/py/parser.py
+++ omniglot/py/parser.py
@@ -1,28 +1,88 @@
 import os
-import pprint
+import random
 import shutil
+import zipfile
 
 
-if __name__ == '__main__':
-    src_root1 = "images_background/"
-    src_root2 = "images_evaluation/"
-    dst_root = "data/original/"
+def unzip(target):
+    with zipfile.ZipFile(target, 'r') as z:
+        z.extractall()
+
+
+def convert(src, dst):
     num2class = {}
-    if not os.path.exists(dst_root):
-        os.mkdir(dst_root)
-
-    for src_root in (src_root1, src_root2):
-        for root, dirs, file_names in os.walk(src_root):
-            if len(dirs) == 0:
-                tmp = file_names[0]
-                class_num = int(tmp[:int(tmp.find("_"))])
-                num2class.setdefault(class_num, root)
-    pprint.pprint(num2class)
+    mkdir(dst)
+    for root, dirs, file_names in os.walk(src):
+        if len(dirs) == 0:
+            tmp = file_names[0]
+            class_num = int(tmp[:int(tmp.find('_'))])
+            num2class.setdefault(class_num, root)
 
     for key, value in num2class.items():
-        root, class_name, subclass_name = value.split("/")
-        dst_path = "{}{:04d}_{}_{}".format(dst_root, key, class_name, subclass_name)
-        if not os.path.exists(dst_path):
-            os.mkdir(dst_path)
+        root, class_name, subclass_name = value.split('/')
+        dst_path = '{}{:04d}_{}_{}'.format(dst, key, class_name, subclass_name)
+        mkdir(dst_path)
         for file_name in os.listdir(value):
-            shutil.copy(os.path.join(value, file_name), os.path.join(dst_path, file_name))
+            shutil.copy(os.path.join(value, file_name),
+                        os.path.join(dst_path, file_name))
+
+
+def make_subset(src):
+    root = os.getcwd()
+    src_root = '{}/data/{}/'.format(root, src)
+    dst_root = '{}/data/'.format(root)
+    for num_subset in range(20):
+        print('Create subset: {}/20'.format(num_subset+1))
+        subset_root = '{}subset{}/{}/'.format(dst_root, num_subset, src)
+        mkdir(subset_root)
+        mkdir(subset_root+'train')
+        mkdir(subset_root+'test')
+        class_names = os.listdir(src_root)
+        class_names.sort()
+        for class_name in class_names:
+            mkdir('{}train/{}'.format(subset_root, class_name))
+            mkdir('{}test/{}'.format(subset_root, class_name))
+            file_names = os.listdir(src_root+class_name+'/')
+            for file_name in file_names:
+                if '{:02d}.png'.format(num_subset+1) in file_name:
+                    os.symlink(src_root+class_name+'/'+file_name,
+                               subset_root+'train/'+class_name+'/'+file_name)
+                else:
+                    os.symlink(src_root+class_name+'/'+file_name,
+                               subset_root+'test/'+class_name+'/'+file_name)
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def remove(target):
+    filename, _ = os.path.splitext(target)
+    shutil.rmtree(filename)
+
+if __name__ == '__main__':
+    mkdir('data')
+    zipfile1 = 'images_background.zip'
+    zipfile2 = 'images_evaluation.zip'
+    # Extract unzip files
+    print('Extract zipfile: {}'.format(zipfile1))
+    unzip(zipfile1)
+    print('Extract zipfile: {}'.format(zipfile2))
+    unzip(zipfile2)
+    # Rearranging category order
+    src_root = 'images_background/'
+    dst_root = 'data/' + src_root
+    src_root2 = 'images_evaluation/'
+    dst_root2 = 'data/' + src_root2
+    print('Rearranging category order')
+    convert(src_root, dst_root)
+    convert(src_root2, dst_root2)
+    # Create subset
+    print('Create subset')
+    make_subset('images_background')
+    make_subset('images_evaluation')
+    # Remove unzip files
+    print('Remove unzipfile')
+    remove(zipfile1)
+    remove(zipfile2)
--- omniglot/scripts/start.sh
+++ omniglot/scripts/start.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-mkdir -p data
 wget https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_background.zip
 wget https://raw.githubusercontent.com/brendenlake/omniglot/master/python/images_evaluation.zip
-unzip images_background.zip
-unzip images_evaluation.zip
 python py/parser.py
-python py/symblic.py
-rm -rf images_background
-rm -rf images_evaluation
